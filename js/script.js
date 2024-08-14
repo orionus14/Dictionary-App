eee4efdf91d7022ef4b8c0fc65a2a01de876e12d
orion <orion2003orion@gmail.com>
2024-08-14 14:32:27 +0300

    Showing elements after clicking search button

--- js/script.js
+++ js/script.js
@@ -9,5 +9,20 @@ btn.addEventListener("click", () => {
         .then(response => response.json())
         .then(json => {
             console.log(json);
+            result.innerHTML = `
+            <div class="word">
+                <h3>${inpWord}</h3>
+                <button"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+            <div class="details">
+                <p>${json[0].meanings[0].partOfSpeech}</p>
+                <p>${json[0].phonetics[1].text}</p>
+            </div>
+            <p class="word-meaning">
+                ${json[0].meanings[0].definitions[0].definition}
+            </p>
+            <p class="word-example">
+            ${json[0].meanings[0].definitions[0].example || ""}
+            </p>`;
     })
-});
+})
